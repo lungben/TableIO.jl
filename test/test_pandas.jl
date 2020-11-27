a4e1de22c495f2259847260d4927c501dff82b33
Benjamin Lungwitz <benj.lung@googlemail.com>
2020-11-27 19:26:30 +0100

    HDF5 io via pandas implemented

--- test/test_pandas.jl
+++ test/test_pandas.jl
@@ -1,13 +1,20 @@
 
+# ENV["PYTHON"] = ""
+# using Pkg
+# Pkg.build("PyCall")
+# using Conda
+# Conda.add("pandas pytables")
+
+
 @testset "HDF5" begin
     fname = joinpath(testpath, "test.hdf")
     write_table!(fname, df)
     @test filesize(fname) > 0
     df_recovered = DataFrame(read_table(fname); copycols=false)
-    @test df == df_recovered
+    @test compare_df_ignore_order(df,df_recovered)
     fname = joinpath(testpath, "test2.hdf")
     write_table!(fname, nt)
     @test filesize(fname) > 0
     nt_recovered = read_table(fname)
-    @test DataFrame(nt) == nt_recovered
+    @test compare_df_ignore_order(DataFrame(nt), nt_recovered)
 end
