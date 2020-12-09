f7fbb30bde4ddbaa7edfce1c528524afd26b7bb5
Benjamin Lungwitz <benj.lung@googlemail.com>
2020-12-09 18:13:06 +0100

    jld2 added

---
+++ src/jld2.jl
@@ -0,0 +1,29 @@
+@info "JLD2.jl is available - including functionality to read / write JLD2 files"
+
+import .JLD2
+
+function read_table(::TableIOInterface.JLD2Format, filename:: AbstractString, tablename:: AbstractString; kwargs...)
+    return JLD2.jldopen(filename, "r") do file
+        file[tablename]
+    end
+end
+
+function read_table(::TableIOInterface.JLD2Format, filename:: AbstractString; kwargs...)
+    return JLD2.jldopen(filename, "r") do file
+        table_list = keys(file) |> sort
+        file[first(table_list)]
+    end
+end
+
+function write_table!(::TableIOInterface.JLD2Format, filename:: AbstractString, tablename:: AbstractString, table; kwargs...)
+    JLD2.jldopen(filename, "a+") do file
+        file[tablename] = table
+    end
+    nothing
+end
+
+function list_tables(::TableIOInterface.JLD2Format, filename:: AbstractString)
+    return JLD2.jldopen(filename, "r") do file
+        keys(file) |> sort
+    end
+end
